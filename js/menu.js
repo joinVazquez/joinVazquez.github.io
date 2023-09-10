150579aa1508265176375929b9df51a9ca94e8dc
Join Vazquez <emiliolouback@gmail.com>
2023-09-10 14:16:26 -0300

    10/09/2023

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+document.write (
+
+"<li id='nav1' class='nav-item' style='padding-right: 1.5rem;'><a class='nav-link' </li>",
+"<li id='nav2' ><a class='dropdown-item' href='colecao.html' target='_blank'>COLEÇÃO</a></li>",
+"<li id='nav3' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Categorias </a><div class='dropdown-menu'><a class='dropdown-item' href='packs.html'>Packs</a>",
+"<a class='dropdown-item' href='mockups.html'>Mockups</a>",
+
+
+
+"<li id='nav8'>","<a class='dropdown-item' href='#' role='button' aria-haspopup='true' onclick='logout()'>SAIR</a>",
+"</div>"
+);
+
+
+/*"<li id='nav8' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>PERFIL</a><div class='dropdown-menu'>",
+"<a class='dropdown-item' onclick='logout()'>SAIR</a>",
+"</div>"*/
